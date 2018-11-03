f2442d916697ec4d7194ef6c906999984fa22dbf
Juha Syrjälä <juha.syrjala@iki.fi>
2018-11-03 19:10:36 +0200

    Highlight current player score

--- src/ui.ts
+++ src/ui.ts
@@ -12,9 +12,9 @@ export class Screen {
 
         const playerInfo = players.map((player: any) => {
             const color = `rgb(${player.color.r*255},${player.color.g*255},${player.color.b*255})`
-            if (player.id == this.clientId) {
+            if (player.clientId == this.clientId) {
                 // this is me
-                return `<div style="color: ${color}"><strong>${player.name}: ${player.score}</strong></div>`
+                return `<div style="color: ${color}; text-decoration: underline;"><strong>${player.name}: ${player.score}</strong></div>`
             }
 
             return `<div style="color: ${color}">${player.name}: ${player.score}</div>`
